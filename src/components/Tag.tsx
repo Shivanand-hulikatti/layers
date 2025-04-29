017771306959d9a356a11569e14c787b75181e39
Shivanand-hulikatti <01fe23bcs268@kletech.ac.in>
2025-04-29 16:28:09 +0530

    more sections added

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,7 +1,13 @@
+import { HTMLAttributes } from "react"
+import { twMerge } from "tailwind-merge"
 
-const Tag = () => {
+const Tag = (props:HTMLAttributes<HTMLDivElement>) => {
+    const {className,children,...rest} = props 
   return (
-    <div>Tag</div>
+    <div className={twMerge('inline-flex border border-lime-400 px-3 py-1 gap-2 rounded-full uppercase items-center',className)} {...rest}>
+        <span>&#10038;</span>
+        <span className="text-sm ">{children}</span>
+    </div>
   )
 }
 
