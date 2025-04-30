9f0595398427aab31bebc0f3669c1d16e2cbfd12
Shivanand-hulikatti <01fe23bcs268@kletech.ac.in>
2025-04-30 21:38:36 +0530

    more animations added

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -3,6 +3,7 @@ import Tag from "@/components/Tag";
 import avatar1 from "@/assets/images/avatar-ashwin-santiago.jpg";
 import avatar2 from "@/assets/images/avatar-florence-shaw.jpg";
 import avatar3 from "@/assets/images/avatar-lula-meyers.jpg";
+import avatar4 from "@/assets/images/avatar-owen-garcia.jpg";
 import Image from "next/image";
 import Avatar from "@/components/Avatar";
 import Key from "@/components/Key";
@@ -32,7 +33,7 @@ export default function Features() {
                     <FeaturesCard
                         title="Real-time collaboration"
                         description="Work together seamlessly with confict-free team editing"
-                        className="md:col-span-2 lg:col-span-1"
+                        className="md:col-span-2 lg:col-span-1 group"
                     >
                         <div className="aspect-video flex justify-center items-center">
                             <Avatar className="z-40">
@@ -56,12 +57,18 @@ export default function Features() {
                                     className="rounded-full"
                                 />
                             </Avatar>
-                            <Avatar className="-ml-6 border-transparent">
-                                <div className="bg-neutral-700 size-full rounded-full inline-flex items-center justify-center gap-1">
-                                    {Array.from({length:3}).map((_,i)=>(
-                                        <span className="size-1.5 rounded-full bg-white" key={i}>
-
-                                        </span>
+                            <Avatar className="-ml-6 border-transparent group-hover:border-green-500 transition">
+                                <div className="bg-neutral-700 size-full rounded-full inline-flex items-center justify-center gap-1 relative">
+                                    <Image
+                                        src={avatar4}
+                                        alt="Avatar 4"
+                                        className="absolute size-full rounded-full opacity-0 group-hover:opacity-100 transition"
+                                    />
+                                    {Array.from({ length: 3 }).map((_, i) => (
+                                        <span
+                                            className="size-1.5 rounded-full bg-white"
+                                            key={i}
+                                        ></span>
                                     ))}
                                 </div>
                             </Avatar>
@@ -72,9 +79,21 @@ export default function Features() {
                         description="Engage your clients with prototypes that reacts to user actions"
                         className="md:col-span-2 lg:col-span-1"
                     >
-                        <div className="aspect-video flex justify-center items-center"> 
+                        <div className="aspect-video flex justify-center items-center">
                             <p className="text-4xl font-extrabold  text-white/20 text-center">
-                            We&apos;ve achieved <span className="bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent">incredible</span> growth this year 
+                                We&apos;ve achieved{" "}
+                                <span className="relative bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent">
+                                    <span>incredible</span>
+                                    <video
+                                        src="/assets/gif-incredible.mp4"
+                                        autoPlay
+                                        muted
+                                        playsInline
+                                        loop
+                                        className="absolute bottom-full left-1/2 -translate-x-1/2 rounded-2xl"
+                                    ></video>
+                                </span>{" "}
+                                growth this year
                             </p>
                         </div>
                     </FeaturesCard>
@@ -84,17 +103,24 @@ export default function Features() {
                         className="md:col-span-2 md:col-start-2 lg:col-span-1 lg:col-start-auto"
                     >
                         <div className="aspect-video flex justify-center items-center gap-4">
-                             <Key className="w-28">Shift</Key>
-                             <Key>alt</Key>
-                             <Key>C</Key>
+                            <Key className="w-28">Shift</Key>
+                            <Key>alt</Key>
+                            <Key>C</Key>
                         </div>
                     </FeaturesCard>
                 </div>
                 <div className="mt-8 flex flex-wrap gap-3 justify-center">
                     {features.map((feature) => (
-                        <div key={feature} className="bg-neutral-900 border-white/10 inline-flex px-3  md:px-5  py-1.5 md:py-2 rounded-2xl gap-3 items-center">
-                            <span className=" bg-lime-400 text-neutral-950 size-5 inline-flex justify-center items-center rounded-full">&#10038;</span>
-                            <span className="font-medium md:text-lg">{feature}</span>
+                        <div
+                            key={feature}
+                            className="bg-neutral-900 border-white/10 inline-flex px-3  md:px-5  py-1.5 md:py-2 rounded-2xl gap-3 items-center"
+                        >
+                            <span className=" bg-lime-400 text-neutral-950 size-5 inline-flex justify-center items-center rounded-full">
+                                &#10038;
+                            </span>
+                            <span className="font-medium md:text-lg">
+                                {feature}
+                            </span>
                         </div>
                     ))}
                 </div>
