be91e6b531f5ae444d99a3e7591a65c009174d05
Shivanand-hulikatti <01fe23bcs268@kletech.ac.in>
2025-05-02 13:22:02 +0530

    2 final commit

--- src/components/Key.tsx
+++ src/components/Key.tsx
@@ -2,13 +2,14 @@ import React, { HTMLAttributes } from "react";
 import { twMerge } from "tailwind-merge";
 
 const Key = (props: HTMLAttributes<HTMLDivElement>) => {
-    const { className, children } = props;
+    const { className, children, ...rest } = props;
     return (
         <div
             className={twMerge(
                 "size-14 bg-neutral-300 inline-flex items-center justify-center text-neutral-950 text-xl rounded-2xl font-medium",
                 className
             )}
+            {...rest}
         >
             {children}
         </div>
