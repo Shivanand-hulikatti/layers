442a08e401a773ddf66c4ca129e06df287eab383
Shivanand-hulikatti <01fe23bcs268@kletech.ac.in>
2025-05-02 12:42:12 +0530

    faq's animation

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,18 +1,30 @@
+"use client";
+
+import { motion } from "framer-motion";
+
 export default function CallToAction() {
-    return <section className="py-24">
-        <div className="overflow-x-clip p-4 flex">
-            <div className="flex flex-none gap-16 text-7xl md:text-8xl font-medium">
-                {Array.from({length:10},(_,i)=>(
-                    <div key={i} className="flex items-center gap-16">
-                        <span className="text-lime-400 text-7xl ">
-                            &#10038;
-                        </span>
-                        <span>
-                            Try it for free
-                        </span>
-                    </div>
-                ))}  
-            </div> 
-        </div>         
-    </section>;
+    return (
+        <section className="py-24">
+            <div className="overflow-x-clip p-4 flex">
+                <motion.div
+                    animate={{ x: "-50%" }}
+                    transition={{
+                        duration: 30,
+                        ease: "linear",
+                        repeat: Infinity,
+                    }}
+                    className="flex flex-none gap-16 text-7xl md:text-8xl font-medium"
+                >
+                    {Array.from({ length: 10 }, (_, i) => (
+                        <div key={i} className="flex items-center gap-16">
+                            <span className="text-lime-400 text-7xl ">
+                                &#10038;
+                            </span>
+                            <span>Try it for free</span>
+                        </div>
+                    ))}
+                </motion.div>
+            </div>
+        </section>
+    );
 }
--- src/sections/Faqs.tsx
+++ src/sections/Faqs.tsx
@@ -2,7 +2,7 @@
 import Tag from "@/components/Tag";
 import { useState } from "react";
 import { twMerge } from "tailwind-merge";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 const faqs = [
     {
@@ -40,13 +40,16 @@ export default function Faqs() {
                     Questions? We&apos;ve got{" "}
                     <span className="text-lime-400">answers</span>
                 </h2>
-                <motion.div className="mt-12 flex flex-col gap-6 max-w-xl mx-auto">
+                <div className="mt-12 flex flex-col gap-6 max-w-xl mx-auto">
                     {faqs.map((faq, faqIndex) => (
                         <div
                             key={faq.question}
                             className="bg-neutral-900 rounded-2xl border border-white/10 p-6"
                         >
-                            <div className="flex items-center justify-between">
+                            <div
+                                className="flex items-center justify-between"
+                                onClick={() => setSelectedIndex(faqIndex)}
+                            >
                                 <h3 className="font-medium">{faq.question}</h3>
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -59,7 +62,7 @@ export default function Faqs() {
                                     strokeLinecap="round"
                                     strokeLinejoin="round"
                                     className={twMerge(
-                                        "feather feather-plus text-lime-400 flex-shrink-0",
+                                        "feather feather-plus text-lime-400 flex-shrink-0 transition duration-300",
                                         selectedIndex === faqIndex &&
                                             "rotate-45"
                                     )}
@@ -68,17 +71,34 @@ export default function Faqs() {
                                     <line x1="5" y1="12" x2="19" y2="12"></line>
                                 </svg>
                             </div>
-                            <div
-                                className={twMerge(
-                                    "mt-6",
-                                    selectedIndex !== faqIndex && "hidden"
+                            <AnimatePresence>
+                                {selectedIndex === faqIndex && (
+                                    <motion.div
+                                        initial={{
+                                            height: 0,
+                                            marginTop: 0,
+                                        }}
+                                        animate={{
+                                            height: "auto",
+                                            marginTop: 24,
+                                        }}
+                                        exit={{
+                                            height: 0,
+                                            marginTop: 0,
+                                        }}
+                                        className={twMerge(
+                                            "mt-6 overflow-hidden"
+                                        )}
+                                    >
+                                        <p className="text-white/50">
+                                            {faq.answer}
+                                        </p>
+                                    </motion.div>
                                 )}
-                            >
-                                <p className="text-white/50">{faq.answer}</p>
-                            </div>
+                            </AnimatePresence>
                         </div>
                     ))}
-                </motion.div>
+                </div>
             </div>
         </section>
     );
